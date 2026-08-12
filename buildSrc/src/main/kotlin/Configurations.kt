6f530a19a4e4a7d70d538ef04bc5d81aba1b9bde
AX <ax@localhost>
2026-08-12 09:11:55 +0000

    chore: bump version to v4.4.9

--- buildSrc/src/main/kotlin/Configurations.kt
+++ buildSrc/src/main/kotlin/Configurations.kt
@@ -4,7 +4,7 @@ object Configurations {
   const val minSdk = 26
   const val majorVersion = 4
   const val minorVersion = 4
-  const val patchVersion = 8
+  const val patchVersion = 9
   const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-  const val versionCode = 37
+  const val versionCode = 38
 }
