ea5b41397b6dfa28409fe5cf4d469ca9d5bdc095
AX <ax@localhost>
2026-08-21 22:31:32 +0000

    chore: bump version to 4.6.1 (versionCode 45)

--- buildSrc/src/main/kotlin/Configurations.kt
+++ buildSrc/src/main/kotlin/Configurations.kt
@@ -4,7 +4,7 @@ object Configurations {
   const val minSdk = 26
   const val majorVersion = 4
   const val minorVersion = 6
-  const val patchVersion = 0
+  const val patchVersion = 1
   const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-  const val versionCode = 44
+  const val versionCode = 45
 }
